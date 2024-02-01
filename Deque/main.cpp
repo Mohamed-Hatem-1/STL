3d6e62c32b0482925dc7c53ddb9ad1143419769c
Mohamed Hatem <mo.hatem1911@gmail.com>
2024-02-01 03:12:57 +0200

    Update main.cpp

--- Deque/main.cpp
+++ Deque/main.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-    // Same as vector but fast insert and delete from the beginning too
+    // Same as vector but fast insert and delete from the beginning too --> O(1)
 
     deque<char>d = {'r', 'o', 'g', 'r', 'a', 'm', 'm', 'e'};
     d.push_back('r'); // = d.emplace_back()
     d.push_front('p');
-    for (int i = 0; i < d.size(); ++i) cout << d[i] << " "; cout << "\n";
-    cout << d.front() << "\n"; // = d[0]
-    cout << d.back() << "\n"; // = d[d.size() - 1]
+    for (int i = 0; i < d.size(); ++i) cout << d[i]; cout << "\n"; // programmer
+    cout << d.front() << "\n"; // = d[0] // p
+    cout << d.back() << "\n"; // = d[d.size() - 1] // r
     d.pop_front();
     d.pop_back();
-    cout << d.front() << "\n";
-    cout << d.back() << "\n";
+    cout << d.front() << "\n"; // r
+    cout << d.back() << "\n"; // e
     return 0;
 }
