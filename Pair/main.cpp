a42b9ae82af51d0e324307019579b5db2ee18d97
Mohamed Hatem <mo.hatem1911@gmail.com>
2024-02-02 18:57:35 +0200

    Update main.cpp

--- Pair/main.cpp
+++ Pair/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     pair<int,string> p = {12, "ali"};
-    cout << p.first << " ";
+    cout << p.first << " "; // 12
     p.second = "mohamed";
-    cout << p.second << "\n";
+    cout << p.second << "\n"; // mohamed
 
     // to take input from user
     cin >> p.first >> p.second;
@@ -17,10 +17,10 @@ int main() {
     p1.first = "mohamed";
     p1.second.first = 50;
     p1.second.second = 12.25;
-    cout << p1.first << " " << p1.second.first << " " << p1.second.second << "\n";
+    cout << p1.first << " " << p1.second.first << " " << p1.second.second << "\n"; // mohamed 50 12.25
     cout << "**************************************************\n";
 
-    // Vector take pairs
+    // Vector of pairs
     vector<pair<int,string>> vp ;
     vp.emplace_back(1, "ali");
     vp.push_back({2, "mohamed"});
@@ -30,6 +30,6 @@ int main() {
         vp.emplace_back(a, b);
     }
     vp[1].second = "sameh";
-    for (int i = 0; i < vp.size(); ++i) cout << vp[i].first << " " << vp[i].second << "\n";
+    for (auto it : vp) cout << it.first << " " << it.second << "\n";
     return 0;
 }
